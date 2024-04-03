5be9b2afe41209c22538f85c3d1a615117a7e155
YuhongCh <73137766+YuhongCh@users.noreply.github.com>
2024-04-03 10:30:36 -0400

    Update boid.py

--- boid.py
+++ boid.py
@@ -1,31 +1,29 @@
 import taichi as ti
-import math
+import numpy as np
 
 ti.init(arch=ti.gpu)
 
 N = 10000
-length_bound = 5000
+length_bound = 100
 centering_factor = 0.1
 repulsion_factor = 0.03
 matching_factor = 0.1
 speed_limit = 5.0
 perception_radius = 7.0
-avoidance_radius =2.0
+avoidance_radius = 2.0
 
 boundary_lines = 12
-
 
 particles_pos = ti.Vector.field(3, dtype=ti.f32, shape=N)
 particles_vel = ti.Vector.field(3, dtype=ti.f32, shape=N)
 line_starts = ti.Vector.field(3, dtype=ti.f32, shape=boundary_lines)
 line_ends = ti.Vector.field(3, dtype=ti.f32, shape=boundary_lines)
 
-
-particleArrayIndices = ti.field(dtype=ti.i32, shape=N) # What index in pars_pos and parts_vel represents this particle?
-particleGridIndices = ti.field( dtype=ti.i32, shape=N) #  What grid cell is this particle in?
+particleArrayIndices = ti.field(dtype=ti.i32, shape=N)  # What index in pars_pos and parts_vel represents this particle?
+particleGridIndices = ti.field(dtype=ti.i32, shape=N)  # What grid cell is this particle in?
 # assuming the particleArrayIndices is already sorted
-gridCellWidth = 3.0 * perception_radius
-halfSideCount = int(length_bound / gridCellWidth) + 1
+gridCellWidth = 2.0 * perception_radius
+halfSideCount = int((length_bound / gridCellWidth)) + 1
 gridSideCount = 2 * halfSideCount
 gridCellCount = gridSideCount * gridSideCount * gridSideCount
 gridInverseCellWidth = 1.0 / gridCellWidth
@@ -41,11 +39,13 @@ windows_y = 600
 length_bound = 100
 camera_move_speed = window_x / length_bound * 0.1
 
+
 @ti.kernel
 def init_particles():
     for i in range(N):
         particles_pos[i] = ti.Vector([ti.random() * length_bound for _ in range(3)])
         particles_vel[i] = ti.Vector([ti.random() * 2 - 1 for _ in range(3)]) * speed_limit
+
 
 # @ti.kernel
 # def init_simulation():
@@ -62,9 +62,10 @@ def init_particles():
 
 @ti.func
 def gridIndex3Dto1D(x: int, y: int, z: int, gridResolution: int):
-    return x + y * gridResolution + z * gridResolution * gridResolution
+    return x + y * gridResolution + z * gridResolution * gridResolution;
 
-@ ti.kernel
+
+@ti.kernel
 def IdentifyCellStartEnd():
     # Identify the start point of each cell in the gridIndices array
     # This is basically a parallel unrolling of a loop that goes
@@ -73,20 +74,22 @@ def IdentifyCellStartEnd():
         cellIndex = particleGridIndices[i]
         if i == 0:
             gridStartIndices[cellIndex] = i
-        if i == N-1:
+        if i == N - 1:
             gridEndIndices[cellIndex] = i
-        cellBefore = particleGridIndices[i-1]
+        cellBefore = particleGridIndices[i - 1]
         if cellBefore != cellIndex:
-            gridEndIndices[cellBefore] = i-1
+            gridEndIndices[cellBefore] = i - 1
             gridStartIndices[cellIndex] = i
+
 
 @ti.kernel
 def ComputeIndices():
     # compute grid and boid indices
     for i in range(N):
         cell_index_3D = ti.math.floor(particles_pos[i] * gridInverseCellWidth)
-        particleGridIndices[i] = gridIndex3Dto1D(cell_index_3D[0],cell_index_3D[1],cell_index_3D[2], gridSideCount)
+        particleGridIndices[i] = gridIndex3Dto1D(cell_index_3D[0], cell_index_3D[1], cell_index_3D[2], gridSideCount)
         particleArrayIndices[i] = i
+
 
 # this function is used to indicate that a cell does not have any boid inside
 @ti.kernel
@@ -94,6 +97,7 @@ def resetIntBuffer():
     for i in range(gridCellCount):
         gridStartIndices[i] = -1
         gridEndIndices[i] = -1
+
 
 @ti.kernel
 def kernUpdatePos():
@@ -111,21 +115,18 @@ def kernUpdatePos():
             elif particles_pos[i][j] > length_bound:
                 particles_pos[i][j] -= length_bound
 
-@ti.func
-def getGridCenter(x: int, y: int, z: int) -> ti.math.vec3:
-    return ti.math.vec3(x, y, z) * gridCellWidth
 
 @ti.kernel
 def updateVelScatteredSearch():
     for i in range(N):
-        velocity_change = ti.Vector([0.,0.,0.])
-        center = ti.Vector([0.,0.,0.])
-        avoid = ti.Vector([0.,0.,0.])
-        follow = ti.Vector([0.,0.,0.])
+        velocity_change = ti.Vector([0., 0., 0.])
+        center = ti.Vector([0., 0., 0.])
+        avoid = ti.Vector([0., 0., 0.])
+        follow = ti.Vector([0., 0., 0.])
         n_neighbor_r1 = 0
         n_neighbor_r3 = 0
         pos_self = particles_pos[i]
-        cur_cell_index_3D = ti.math.floor(particles_pos[i] * gridInverseCellWidth, dtype= ti.i32)
+        cur_cell_index_3D = ti.math.floor(particles_pos[i] * gridInverseCellWidth, dtype=ti.i32)
         # these two variables are used to check the boundary conditions
         check_neg = ti.Vector([0, 0, 0])
         check_pos = ti.Vector([0, 0, 0])
@@ -154,19 +155,14 @@ def updateVelScatteredSearch():
             check_pos[2] = 1
         else:
             check_pos[2] = 0
-        for z in range(cur_cell_index_3D[2] - check_neg[2], cur_cell_index_3D[2]+check_pos[2]+1):
+        for z in range(cur_cell_index_3D[2] - check_neg[2], cur_cell_index_3D[2] + check_pos[2] + 1):
             for y in range(cur_cell_index_3D[1] - check_neg[1], cur_cell_index_3D[1] + check_pos[1] + 1):
                 for x in range(cur_cell_index_3D[0] - check_neg[0], cur_cell_index_3D[0] + check_pos[0] + 1):
-
-                    if ti.math.dot(getGridCenter(x, y, z) - pos_self, particles_vel[i]) < 0.0:
-                        continue
-
-                    neighbor_cell_1D = gridIndex3Dto1D(x,y,z,gridSideCount)
+                    neighbor_cell_1D = gridIndex3Dto1D(x, y, z, gridSideCount)
 
                     if gridStartIndices[neighbor_cell_1D] == -1 or gridEndIndices[neighbor_cell_1D] == -1:
                         # no bird in the cell
                         continue
-
                     for idx in range(gridStartIndices[neighbor_cell_1D], gridEndIndices[neighbor_cell_1D]):
                         index_other = particleArrayIndices[idx]
                         pos_other = particles_pos[index_other]
@@ -181,49 +177,65 @@ def updateVelScatteredSearch():
                                 avoid -= (pos_other - pos_self)
                             if dis < perception_radius:
                                 follow += particles_vel[index_other]
-                                n_neighbor_r3 +=1
-        if n_neighbor_r1 > 0:
-            velocity_change += (center/float(n_neighbor_r1) - pos_self) * centering_factor
-        velocity_change += avoid * repulsion_factor
-        if n_neighbor_r3 > 0:
-            velocity_change += (follow / float(n_neighbor_r3)) * matching_factor
-        # update velocity
-        particles_vel[i] += velocity_change
+                                n_neighbor_r3 += 1
+            if n_neighbor_r1 > 0:
+                velocity_change += (center / float(n_neighbor_r1) - pos_self) * centering_factor
+            velocity_change += avoid * repulsion_factor
+            if n_neighbor_r3 > 0:
+                velocity_change += (follow / float(n_neighbor_r3)) * matching_factor
+                # update velocity
+                particles_vel[i] += velocity_change
 
-@ti.kernel
-def sort_keys():
-    # Radix Sort
-    max_value = 0
-    for i in range(N):
-        max_value = max(max_value, particleGridIndices[i])
 
-    exp = 1
-    while max_value // exp > 0:
-        # Counting Sort
-        count = ti.field(ti.i32, shape=10)
-        output = ti.field(ti.i32, shape=N)
+def sort_keys_scattered():
+    particleArrayIndices_np = particleArrayIndices.to_numpy()
+    particleGridIndices_np = particleGridIndices.to_numpy()
 
-        for i in range(10):
-            count[i] = 0
+    # Get the indices that would sort the particleGridIndices array.
+    sorted_indices = np.argsort(particleGridIndices_np)
 
-        for i in range(N):
-            index = particleGridIndices[i] // exp % 10
-            count[index] += 1
+    # Use the sorted indices to reorder the arrays.
+    sorted_particleGridIndices = particleGridIndices_np[sorted_indices]
+    sorted_particleArrayIndices = particleArrayIndices_np[sorted_indices]
+    particleArrayIndices.from_numpy(sorted_particleArrayIndices)
+    particleGridIndices.from_numpy(sorted_particleGridIndices)
 
-        for i in range(1, 10):
-            count[i] += count[i - 1]
 
-        i = N - 1
-        while i >= 0:
-            index = particleGridIndices[i] // exp % 10
-            output[count[index] - 1] = particleGridIndices[i]
-            count[index] -= 1
-            i -= 1
+def sort_keys_coherent():
+    particleArrayIndices_np = particleArrayIndices.to_numpy()
+    particleGridIndices_np = particleGridIndices.to_numpy()
+    particlesPos_np = particles_pos.to_numpy()
+    particlesVel_np = particles_vel.to_numpy()
+    # Get the indices that would sort the particleGridIndices array.
+    sorted_indices = np.argsort(particleGridIndices_np)
 
-        for i in range(N):
-            particleGridIndices[i] = output[i]
+    # Use the sorted indices to reorder the arrays.
+    sorted_particleGridIndices = particleGridIndices_np[sorted_indices]
+    sorted_particleArrayIndices = particleArrayIndices_np[sorted_indices]
+    sorted_particlesPos = particlesPos_np[sorted_indices]
+    sorted_particlesVel = particlesVel_np[sorted_indices]
+    particleArrayIndices.from_numpy(sorted_particleArrayIndices)
+    particleGridIndices.from_numpy(sorted_particleGridIndices)
+    particles_pos.from_numpy(sorted_particlesPos)
+    particles_vel.from_numpy(sorted_particlesVel)
 
-        exp *= 10
+
+def stepSimulationScatteredGrid():
+    ComputeIndices()
+    sort_keys_scattered()
+    resetIntBuffer()
+    IdentifyCellStartEnd()
+    updateVelScatteredSearch()
+    kernUpdatePos()
+
+
+def stepSimulationCoherentGrid():
+    ComputeIndices()
+    sort_keys_coherent()
+    resetIntBuffer()
+    IdentifyCellStartEnd()
+    updateVelScatteredSearch()
+    kernUpdatePos()
 
 
 @ti.kernel
@@ -262,22 +274,13 @@ def prefixSumApproach():
     kernUpdatePos()
 
 
-def stepSimulationScatteredGrid():
-    ComputeIndices()
-    ti.algorithms.parallel_sort(particleGridIndices, particleArrayIndices)
-    #sort_keys()
-    resetIntBuffer()
-    IdentifyCellStartEnd()
-    updateVelScatteredSearch()
-    kernUpdatePos()
-
-
 @ti.func
 def limit_velocity(velocity):
     result_velocity = velocity  # Default to the original velocity
     if velocity.norm() > speed_limit:
         result_velocity = velocity.normalized() * speed_limit
     return result_velocity
+
 
 @ti.func
 def avoid_other_boids(i):
@@ -286,8 +289,9 @@ def avoid_other_boids(i):
         if i != j:
             distance = (particles_pos[i] - particles_pos[j]).norm()
             if distance < avoidance_radius:  # Avoidance radius
-                repulsion += (particles_pos[i] - particles_pos[j]) / distance
+                repulsion -= (particles_pos[j] - particles_pos[i])
     return repulsion * repulsion_factor
+
 
 @ti.func
 def align_velocity(i):
@@ -301,7 +305,8 @@ def align_velocity(i):
                 count += 1
     if count > 0:
         average_vel /= count
-    return (average_vel - particles_vel[i]) * matching_factor
+    return average_vel * matching_factor
+
 
 @ti.func
 def find_flock_center(i):
@@ -319,27 +324,33 @@ def find_flock_center(i):
         center /= count
     return (center - particles_pos[i]) * centering_factor
 
+
 @ti.kernel
 def update_particles():
     for i in range(N):
         center_offset = find_flock_center(i)
         avoidance_offset = avoid_other_boids(i)
         alignment_offset = align_velocity(i)
-        
+
         velocity = particles_vel[i] + center_offset + avoidance_offset + alignment_offset
         velocity = limit_velocity(velocity)
-        
+
         particles_pos[i] += velocity * 0.1  # Assuming 0.1 as a time step equivalent
         particles_vel[i] = velocity
-        
-        for j in ti.static(range(3)):  
+
+        for j in ti.static(range(3)):
             if particles_pos[i][j] < 0:
-                particles_pos[i][j] += length_bound  
+                particles_pos[i][j] += length_bound
             elif particles_pos[i][j] > length_bound:
-                particles_pos[i][j] -= length_bound  
+                particles_pos[i][j] -= length_bound
+
+            # @ti.kernel
+
+
+
 
 def draw_bounds(x_min=0, y_min=0, z_min=0, x_max=1, y_max=1, z_max=1):
-    box_anchors = ti.Vector.field(3, dtype=ti.f32, shape = 8)
+    box_anchors = ti.Vector.field(3, dtype=ti.f32, shape=8)
     box_anchors[0] = ti.Vector([x_min, y_min, z_min])
     box_anchors[1] = ti.Vector([x_min, y_max, z_min])
     box_anchors[2] = ti.Vector([x_max, y_min, z_min])
@@ -355,8 +366,8 @@ def draw_bounds(x_min=0, y_min=0, z_min=0, x_max=1, y_max=1, z_max=1):
     return box_anchors, box_lines_indices
 
 
-
-SetScatteredGrid = 1
+SetScatteredGrid = 0
+SetCoherentGrid = 1
 init_particles()
 
 # Create a window for rendering
@@ -365,10 +376,10 @@ canvas = window.get_canvas()
 scene = window.get_scene()
 camera = ti.ui.Camera()
 
-camera.position(length_bound/2, length_bound/2, -1.5 *length_bound)  
-camera.lookat(length_bound/2, length_bound/2, 0)    
-box_anchors, box_lines_indices = draw_bounds(x_min=0, y_min=0, z_min=0, x_max=length_bound, y_max=length_bound, z_max=length_bound)
-
+camera.position(length_bound / 2, length_bound / 2, -1.5 * length_bound)
+camera.lookat(length_bound / 2, length_bound / 2, 0)
+box_anchors, box_lines_indices = draw_bounds(x_min=0, y_min=0, z_min=0, x_max=length_bound, y_max=length_bound,
+                                             z_max=length_bound)
 
 while window.running:
     # camera.position(5, 5, -15)  # Set camera position
@@ -377,18 +388,19 @@ while window.running:
     camera.track_user_inputs(window, movement_speed=camera_move_speed, hold_key=ti.ui.RMB)
     scene.set_camera(camera)
     if SetScatteredGrid == 1:
-        prefixSumApproach()
         # stepSimulationScatteredGrid()
+        prefixSumApproach()
+    elif SetCoherentGrid == 1:
+        stepSimulationCoherentGrid()
     else:
         update_particles()
 
-    scene.ambient_light((0.5, 0.5, 0.5)) 
-    # scene.point_light(pos=(0, 10, -10), color=(1, 1, 1)) 
+    scene.ambient_light((0.5, 0.5, 0.5))
+    # scene.point_light(pos=(0, 10, -10), color=(1, 1, 1))
 
     # Render particles
     scene.particles(particles_pos, radius=0.1, color=(0.4, 0.6, 0.9))
-    scene.lines(box_anchors, indices=box_lines_indices, color = (0.99, 0.99, 0.01), width = 2.0)
-    
-    
+    scene.lines(box_anchors, indices=box_lines_indices, color=(0.99, 0.99, 0.01), width=2.0)
+
     canvas.scene(scene)
     window.show()
