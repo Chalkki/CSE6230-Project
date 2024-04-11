27c79ba2ca194c7907071c4b5405d03e9f5f428c
Carl Xiong <34769184+PureDreamerGk@users.noreply.github.com>
2024-04-10 20:12:31 -0400

    Update gpu.cu

--- CSE6230-Project 2/cpp_boids/gpu.cu
+++ CSE6230-Project 2/cpp_boids/gpu.cu
@@ -1,6 +1,9 @@
 #include "common.h"
 #include <cuda.h>
-
+#include <thrust/sort.h>
+#include <thrust/execution_policy.h>
+#include <thrust/random.h>
+#include <thrust/device_vector.h>
 #define NUM_THREADS 256
 
 ///
@@ -108,6 +111,10 @@ __global__ void move_gpu_pos(int N, Vec3 *pos, Vec3 *vel) {
   pos[tid] = thisPos;
 }
 
+__device__ int gridtid3Dto1D(int x, int y, int z, int gridResolution) {
+  return x + y * gridResolution + z * gridResolution * gridResolution;
+}
+
 __global__ void kernUpdateVelocityBruteForce(int N, Vec3 *pos,
     Vec3 *vel1, Vec3 *vel2) {
     // Compute Boid associated with thread
@@ -128,6 +135,101 @@ __global__ void kernUpdateVelocityBruteForce(int N, Vec3 *pos,
     // Record the new velocity into vel2.
     vel2[tid] = new_velocity;
 }
+
+
+__global__ void kernUpdateVelocityScattered(int N, int gridRes, Vec3 gridMin, float inverseCW, float cW, int* startIndex, int* endIndex, int* particleArrayIndex, Vec3 * pos, Vec3* vel1, Vec3* vel2){
+    int tid = threadIdx.x + (blockIdx.x * blockDim.x);
+    if (tid >= N) {
+        return;
+    }
+    
+    Vec3 pos_self = pos[tid];
+    Vec3 grid_c = inverseCW * (pos_self - gridMin);
+    Vec3 grid_c_int = Vec3::floor(grid_c);
+    Vec3 grid_c_frac = grid_c-grid_c_int;
+    
+    Vec3 neg_c;
+    Vec3 pos_c;
+    
+    neg_c.x = (grid_c_frac.x <= 0.5f && grid_c_int.x > 0) ? 1.0f : 0.0f;
+    neg_c.y = (grid_c_frac.y <= 0.5f && grid_c_int.y > 0) ? 1.0f : 0.0f;
+    neg_c.z = (grid_c_frac.z <= 0.5f && grid_c_int.z > 0) ? 1.0f : 0.0f;
+    pos_c.x = (grid_c_frac.x > 0.5f && grid_c_int.x < gridRes-1) ? 1.0f : 0.0f;
+    pos_c.y = (grid_c_frac.y > 0.5f && grid_c_int.y < gridRes-1) ? 1.0f : 0.0f;
+    pos_c.z = (grid_c_frac.z > 0.5f && grid_c_int.z < gridRes-1) ? 1.0f : 0.0f;
+    
+    Vec3 velocity_change;
+    Vec3 perceived_center;
+    Vec3 c;
+    Vec3 perceived_velocity;
+    
+    unsigned int num_neighbors_r1 = 0;
+    unsigned int num_neighbors_r3 = 0;
+    
+    for(int z = grid_c_int.z -  neg_c.z;  z <= grid_c_int.z + pos_c.z; z++){
+        for(int y = grid_c_int.y -  neg_c.y;  y <= grid_c_int.y + pos_c.y; y++){
+            for(int x = grid_c_int.x -  neg_c.x;  x <= grid_c_int.x + pos_c.x; x++){
+                int neigh_id = gridtid3Dto1D(x,y,z, gridRes);
+                if(startIndex[neigh_id] == -1){
+                    continue;
+                }
+                for(int c_ind = startIndex[neigh_id]; c_ind <= endIndex[neigh_id]; c_ind++){
+                    int other_boids  =  particleArrayIndex[c_ind];
+                    Vec3 pos_other = pos[other_boids];
+                    
+                    if(other_boids != tid){
+                    
+                        float dist_to_other = norm(pos_other - pos_self);
+
+                        if (dist_to_other < perception_radius)
+                        {
+                            perceived_center += pos_other;
+                            num_neighbors_r1++;
+                        }
+                
+                        // Rule 2: boids try to stay a distance d away from each other
+                        if (dist_to_other < avoidance_radius)
+                        {
+                            c -= (pos_other - pos_self);
+                        }
+                
+                        // Rule 3: boids try to match the speed of surrounding boids
+                        if (dist_to_other < perception_radius)
+                        {
+                            perceived_velocity += vel1[other_boids];
+                            num_neighbors_r3++;
+                        }
+                    }
+                }
+            }
+        }
+    }
+        
+    if (num_neighbors_r1 > 0)
+    {
+        velocity_change += (perceived_center / (float) num_neighbors_r1 - pos_self) * centering_factor;
+    }
+
+    velocity_change += c * repulsion_factor;
+
+    if (num_neighbors_r3 > 0)
+    {
+        velocity_change += (perceived_velocity / (float) num_neighbors_r3) * matching_factor;
+    }
+    
+    
+    Vec3 new_velocity = vel1[tid] + velocity_change;
+    
+    if (norm(new_velocity) > speed_limit)
+    {
+        new_velocity = speed_limit * normalize(new_velocity);
+    }
+
+    // Record the new velocity into vel2.
+    vel2[tid] = new_velocity; 
+
+}
+
 
 void init_simulation(Vec3 * pos, int num_parts) {
 
@@ -155,13 +257,48 @@ void init_simulation(Vec3 * pos, int num_parts) {
         
 }
 
-__device__ int gridtid3Dto1D(int x, int y, int z, int gridResolution) {
-  return x + y * gridResolution + z * gridResolution * gridResolution;
+
+
+__global__ void computeIndices(int num_parts, int gridres, Vec3 min, float gridInverseCellWidth, Vec3* position, int* indices, int* gridIndex){
+    int tid = threadIdx.x + (blockIdx.x * blockDim.x);
+    if (tid >= num_parts) {
+        return;
+    }
+    
+    Vec3 cell_3D = Vec3::floor(gridInverseCellWidth * (position[tid]-min));
+    gridIndex[tid] = gridtid3Dto1D(cell_3D.x, cell_3D.y, cell_3D.z, gridres);
+    
+    indices[tid] = tid;
+
+}
+
+__global__ void bufferReset(int num_parts,int* Buffer, int val){
+    int tid = threadIdx.x + (blockIdx.x * blockDim.x);
+    if (tid < num_parts) {
+        Buffer[tid] = val;
+    }
+
+}
+
+__global__ void identifyCellInfo(int num_parts, int* gridIndex, int* gridCellStart, int* gridCellEnd){
+    int tid = threadIdx.x + (blockIdx.x * blockDim.x);
+    if (tid >= num_parts) {
+        return;
+    }
+    
+    int part_index = gridIndex[tid];
+    if((tid ==0) || (part_index != gridIndex[tid-1])){
+        gridCellStart[part_index] = tid;
+    }
+    if((tid == num_parts-1) || (part_index != gridIndex[tid+1])){
+        gridCellEnd[part_index] = tid;
+    }
+
 }
 
 void simulate_one_step_naive(Vec3 * pos, int num_parts) {
     kernUpdateVelocityBruteForce <<<blks, NUM_THREADS >>>(num_parts, pos, dev_vel1, dev_vel2);
-    move_gpu_pos<<<blks, NUM_THREADS>>>(num_parts, dev_pos2, dev_vel2);
+    move_gpu_pos<<<blks, NUM_THREADS>>>(num_parts, pos, dev_vel2);
     // ping-pong the velocity buffers
     Vec3* temp = dev_vel2;
     dev_vel2 = dev_vel1;
@@ -173,12 +310,29 @@ void stepSimulationCoherentGrid(Vec3 * pos, int num_parts) {
     // Rule 1: boids fly towards their local perceived center of mass, which excludes themselves
     // Rule 2: boids try to stay a distance d away from each other
     // Rule 3: boids try to match the speed of surrounding boids
+    
 }
 
 
 
 void stepSimulationScatteredGrid(Vec3 * pos, int num_parts) {
-  
+    //int block_per_grid = (num_parts + NUM_THREADS - 1) / NUM_THREADS;
+    //int block_per_cell = (gridCellCount + NUM_THREADS - 1) / NUM_THREADS;
+    //computeIndices <<<block_per_grid, NUM_THREADS>>>(num_parts, gridSideCount, gridMinimum,gridInverseCellWidth,dev_pos2, dev_particleArrayIndices,dev_particleGridIndices);
+    
+    dev_thrust_particleGridIndices = thrust::device_ptr<int>(dev_particleGridIndices);
+    dev_thrust_particleArrayIndices = thrust::device_ptr<int>(dev_particleArrayIndices);
+    //thrust::sort_by_key(dev_thrust_particleGridIndices, dev_thrust_particleGridIndices+num_parts, dev_thrust_particleArrayIndices);
+    
+    //bufferReset<<<block_per_cell,NUM_THREADS>>>(gridCellCount, dev_gridCellStartIndices, -1);
+    //identifyCellInfo<<<block_per_grid, NUM_THREADS>>> (num_parts, dev_particleGridIndices, dev_gridCellStartIndices, dev_gridCellEndIndices);
+    
+    //kernUpdateVelocityScattered <<<block_per_grid, NUM_THREADS >>>(num_parts, gridSideCount, gridMinimum, gridInverseCellWidth, gridCellWidth, dev_gridCellStartIndices, dev_gridCellEndIndices, dev_particleArrayIndices, dev_pos2, dev_vel1, dev_vel2);
+    //move_gpu_pos<<<block_per_grid, NUM_THREADS>>>(num_parts, pos, dev_vel2);
+    // ping-pong the velocity buffers
+    //Vec3* temp = dev_vel2;
+    //dev_vel2 = dev_vel1;
+    //dev_vel1 = temp;
 
 }
 
